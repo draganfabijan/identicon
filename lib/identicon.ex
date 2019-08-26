bca56fcbadfedd566112ac930418b249e72e393b
draganfabijan <dragan.fabijan@gmail.com>
2019-08-26 20:25:30 +1000

    Hashing a string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,13 @@
 defmodule Identicon do
+  def main(input) do
+    input |>
+    hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
 end
