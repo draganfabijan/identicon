0d91e232937a82f4781c3fa8a634e9c4615c403b
draganfabijan <dragan.fabijan@gmail.com>
2019-08-26 20:37:28 +1000

    BUild grod

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,7 +2,23 @@ defmodule Identicon do
   def main(input) do
     input |>
     hash_input |>
-    pick_color
+    pick_color |>
+    build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second, _tail] = row) do # helper method
+    row ++ [second, first]
   end
 
   # can PM multiple args -> def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image, {height, width} = size) do #pick_color({2,3}) where tuple is a second argument
