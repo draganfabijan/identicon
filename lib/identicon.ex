6ad8b8fab346e36286d62a9b6a1c8f1377556c86
draganfabijan <dragan.fabijan@gmail.com>
2019-08-26 20:35:02 +1000

    Pick color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,7 +1,13 @@
 defmodule Identicon do
   def main(input) do
     input |>
-    hash_input
+    hash_input |>
+    pick_color
+  end
+
+  # can PM multiple args -> def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image, {height, width} = size) do #pick_color({2,3}) where tuple is a second argument
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
